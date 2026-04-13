7e010d69ca1a28143b7f0edb3806155daa3c0594
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-04-13 16:15:26 -0500

    feat: crear entidad Movimiento y crear enum TipoMovimientos

---
+++ src/main/java/org/ingresosYgastos/entity/Movimientos.java
@@ -0,0 +1,81 @@
+package org.ingresosYgastos.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Slf4j
+@Entity
+@NoArgsConstructor
+@Getter
+public class Movimientos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private BigDecimal monto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipoMovimiento")
+    private TipoMovimiento tipoMovimiento;
+    private String detalle;
+
+
+    @CreatedDate
+    @Column(name = "fecha")
+    private LocalDateTime fechaRegistro;
+
+
+    private Movimientos(MovimientoBuilder builder) {
+
+        if (builder.monto == null) throw new IllegalArgumentException("El monto es obligatorio");
+        if (builder.tipoMovimiento == null)  throw new IllegalArgumentException("El tipoMovimiento es obligatorio");
+
+
+
+        this.monto = builder.monto;
+        this.tipoMovimiento = builder.tipoMovimiento;
+        this.detalle = builder.detalle;
+        this.fechaRegistro = LocalDateTime.now();
+
+    }
+
+
+
+    public static class MovimientoBuilder {
+        private BigDecimal monto;
+        private TipoMovimiento tipoMovimiento;
+        private String detalle;
+
+
+
+        public MovimientoBuilder() {}
+
+        public MovimientoBuilder monto(BigDecimal monto) {
+            this.monto = monto;
+            return this;
+        }
+
+        public MovimientoBuilder tipo(TipoMovimiento tipoMovimiento) {
+            this.tipoMovimiento = tipoMovimiento;
+            return this;
+        }
+
+        public MovimientoBuilder detalle(String detalle) {
+            this.detalle = detalle;
+            return this;
+        }
+
+
+        public Movimientos registroBuild() {
+            return new Movimientos(this);
+        }
+
+    }
+}
---
+++ src/main/java/org/ingresosYgastos/entity/TipoMovimiento.java
@@ -0,0 +1,6 @@
+package org.ingresosYgastos.entity;
+
+public enum TipoMovimiento {
+    INGRESO,
+    GASTO
+}
