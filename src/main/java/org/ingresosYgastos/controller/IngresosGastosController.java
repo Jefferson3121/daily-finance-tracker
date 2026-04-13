4b72ee766dc93cab6fdff8b22164bf653cab1473
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-04-13 16:44:11 -0500

    feact: agregar metodo get para obtener reumen de movimientos

--- src/main/java/org/ingresosYgastos/controller/IngresosGastosController.java
+++ src/main/java/org/ingresosYgastos/controller/IngresosGastosController.java
@@ -1,28 +1,40 @@
 package org.ingresosYgastos.controller;
 
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ingresosYgastos.dto.RegistrarRequestDTO;
+import org.ingresosYgastos.dto.ResumenResponseDTO;
 import org.ingresosYgastos.services.IngresosGastosService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/registrar")
+@RequestMapping(("/movimientos  "))
 public class IngresosGastosController {
 
     private final IngresosGastosService ingresosGastosService;
 
 
     @PostMapping
-    public void registrar(@RequestBody  RegistrarRequestDTO requestDTO){
+    public void registrar(@Valid @RequestBody  RegistrarRequestDTO requestDTO){
         ingresosGastosService.registrar(requestDTO);
+    }
+
+
+    @GetMapping("/resumen")
+    public ResponseEntity<ResumenResponseDTO> obtenerResumen(@RequestParam LocalDate fechaInicio, @RequestParam LocalDate fechaFin){
+
+        ResumenResponseDTO resumen = ingresosGastosService.ontenerResumen(fechaInicio, fechaFin);
+
+        return ResponseEntity.status(HttpStatus.OK).body(resumen);
     }
 
 
