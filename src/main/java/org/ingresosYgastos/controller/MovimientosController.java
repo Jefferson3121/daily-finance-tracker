4b1bb82c5563d3f57d7bf3658a1ebe730a8e7fff
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-04-14 17:37:12 -0500

    feat: agregar metodo endpoint para listar movimientos entre periodos de tiempo

--- src/main/java/org/ingresosYgastos/controller/MovimientosController.java
+++ src/main/java/org/ingresosYgastos/controller/MovimientosController.java
@@ -4,20 +4,22 @@ package org.ingresosYgastos.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ingresosYgastos.dto.EditarMovimientoRequestDTO;
+import org.ingresosYgastos.dto.MovimientoResponseDTO;
 import org.ingresosYgastos.dto.RegistrarRequestDTO;
 import org.ingresosYgastos.dto.ResumenResponseDTO;
+import org.ingresosYgastos.entity.Movimientos;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(("/movimientos  "))
+@RequestMapping(("/movimientos"))
 public class MovimientosController {
 
     private final org.ingresosYgastos.services.MovimientosService movimientosService;
@@ -40,7 +42,17 @@ public class MovimientosController {
 
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
-        movimientosService.eliminar(id);
+        movimientosService.eliminarMovimiento(id);
+    }
+
+
+
+    @GetMapping
+    public List<MovimientoResponseDTO> listarMovimientos(
+            @RequestParam LocalDate fechaInicio,
+            @RequestParam LocalDate fechaFin) {
+
+        return movimientosService.listarMovimientos(fechaInicio, fechaFin);
     }
 
 
